7e5b8ee13f1db85c0fdcdb3f2fdd53b2b0d51aa0
ilya <ilia.arkhangelsky@gmail.com>
2022-12-16 15:59:29 +0300

    рабочая вервия программы, которая проверяет два числа и выдает большее и меньшее

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 ->  max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+string userInput_A = Console.ReadLine() ?? "";
+Console.Write("Введите число 2: ");
+string userInput_B = Console.ReadLine() ?? "";
+
+double numberA = double.Parse(userInput_A);
+double numberB = double.Parse(userInput_B);
+double max = double.Parse(userInput_A);
+double min = double.Parse(userInput_B);
+
+if (numberB > max)
+{
+    max = numberB;
+    min = numberA;
+}
+else
+{
+    max = numberA;
+    min = numberB;
+}
+if (numberA == numberB)
+{
+    Console.WriteLine("Невозможно определить максимальное и минимальное число, так как числа равны ");
+}
+else
+{
+Console.WriteLine("Максимальным из двух числел является: ");
+Console.WriteLine(max);
+Console.WriteLine("Минимальным из двух числел является: ");
+Console.WriteLine(min);
+}
